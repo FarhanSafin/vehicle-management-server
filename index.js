2ce146f5ed2c45d21dabd94a73489191f70f9f58
Farhan <farhansafin@gmail.com>
2022-05-05 12:29:38 +0600

    GET API created and checked

--- index.js
+++ index.js
@@ -16,12 +16,29 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a8ht1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Vehicle DB connected')
-  // perform actions on the collection object
-  client.close();
-});
+
+
+
+async function run () {
+    try{
+        await client.connect();
+        const vehicleCollection = client.db('vehicleList').collection('vehicle');
+
+        app.get('/vehicleList', async (req, res) => {
+            const query = {};
+            const cursor = vehicleCollection.find(query);
+            const collections = await cursor.toArray();
+            res.send(collections);
+        });
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
