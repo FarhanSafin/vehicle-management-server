2449391acdd78f177a41412bf8113aa0adbff804
Farhan <farhansafin@gmail.com>
2022-05-07 02:02:49 +0600

    JWT implemented for email/password and social login for my items page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unathorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(403).send({message: 'Forbidden Access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 //mongodb connection parameters
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a8ht1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -37,12 +53,17 @@ async function run () {
             res.send(collection);
         });
 
-        app.get('/myItems', async (req, res) => {
+        app.get('/myItems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const search = req.query.email;
-            const query = {email:  search };
-            const cursor = vehicleCollection.find(query);
-            const collections = await cursor.toArray();
-            res.send(collections);
+            if(search === decodedEmail){
+                const query = {email:  search };
+                const cursor = vehicleCollection.find(query);
+                const collections = await cursor.toArray();
+                res.send(collections);
+            }else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         });
 
         //patch api
@@ -75,6 +96,15 @@ async function run () {
             const query = {_id: ObjectId(id)};
             const result = await vehicleCollection.deleteOne(query);
             res.send(result);
+        })
+
+        //AUTH
+        app.post('/login', async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
         })
 
     }
